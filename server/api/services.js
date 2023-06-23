5c8a38f712eddfbf6f263f6aa189c1a100dd5596
agcrisbp <agcrisbp@email.com>
2023-06-23 08:05:09 +0700

    v24.0.1

--- server/api/services.js
+++ server/api/services.js
@@ -7,7 +7,7 @@ export default defineEventHandler(() => {
       title: 'React',
       description: {
         en: 'I build modern and advanced websites and applications with React or Expo by React Native.',
-     tr: 'Saya membuat situs web dan aplikasi modern menggunakan React atau Expo dari React Native.',
+     id_ID: 'Saya membuat situs web dan aplikasi modern menggunakan React atau Expo dari React Native.',
       },
       icon: 'devicon:react',
       image: null,
@@ -17,7 +17,7 @@ export default defineEventHandler(() => {
       title: 'Vue',
       description: {
         en: 'I use Vue, a modern JavaScript and Typescript framework, while making dynamic and static websites and applications.',
-     tr: 'Terkadang saya juga menggunakan Vue, framework JavaScript dan Typescript sumber terbuka modern, ketika membuat website maupun aplikasi.',
+     id_ID: 'Terkadang saya juga menggunakan Vue, framework JavaScript dan Typescript sumber terbuka modern, ketika membuat website maupun aplikasi.',
       },
       icon: 'devicon:vuejs',
       image: null,
@@ -27,7 +27,7 @@ export default defineEventHandler(() => {
       title: 'Kotlin',
       description: {
         en: 'For an app, I also use Kotlin.',
-     tr: 'Untuk membuat aplikasi, saya juga menggunakan Kotlin.',
+     id_ID: 'Untuk membuat aplikasi, saya juga menggunakan Kotlin.',
       },
       icon: 'devicon:kotlin',
       image: null,
@@ -37,7 +37,7 @@ export default defineEventHandler(() => {
       title: 'Python',
       description: {
         en: 'In Cybersecurity, I use Python, SQL, Java/JavaScript, C++, etc. But Python is the language I use more often.',
-     tr: 'Dalam bidang Keamanan Siber, saya menggunakan Python, SQL, Java/JavaScript, C++, dll. Tapi Python adalah bahasa yang lebih sering saya gunakan.',
+     id_ID: 'Dalam bidang Keamanan Siber, saya menggunakan Python, SQL, Java/JavaScript, C++, dll. Tapi Python adalah bahasa yang lebih sering saya gunakan.',
       },
       icon: 'devicon:python',
       image: null,
