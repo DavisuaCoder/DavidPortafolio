f1e41a336d8e8d2d587ab609f5e95a4fb3d6e601
David Alvarez <davisua.stuff@gmail.com>
2023-07-25 20:52:48 +0200

    partial fixed hover and scroll in navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,26 +2,22 @@
   <nav class="navbar" :class="{ 'navbar-scroll': isScrolled }">
     <ul class="navbar-list">
       <li class="navbar-item">
-        <a href="#about" class="navbar-link" :class="{ 'navbar-link-active': activeSection === 'about' }" @click="setActiveSection('about')">
+        <a href="#about" class="navbar-link" :class="{ 'active': activeSection === 'about' }" >
           {{ $t('pageTitles.about') }}
         </a>
       </li>
-
       <li class="navbar-item">
-        <a href="#resume" class="navbar-link" :class="{ 'navbar-link-active': activeSection === 'resume' }" @click="setActiveSection('resume')">
+        <a href="#resume" class="navbar-link" :class="{ 'active': activeSection === 'resume' }">
           {{ $t('pageTitles.resume') }}
         </a>
       </li>
-
-      
       <li class="navbar-item">
-        <a href="#github" class="navbar-link" :class="{ 'navbar-link-active': activeSection === 'github' }" @click="setActiveSection('github')">
+        <a href="#github" class="navbar-link" :class="{ 'active': activeSection === 'github' }">
           {{ $t('pageTitles.github') }}
         </a>
       </li>
-
       <li class="navbar-item">
-        <a href="#portfolio" class="navbar-link" :class="{ 'navbar-link-active': activeSection === 'portfolio' }" @click="setActiveSection('portfolio')">
+        <a href="#portfolio" class="navbar-link" :class="{ 'active': activeSection === 'portfolio' }">
           {{ $t('pageTitles.portfolio') }}
         </a>
       </li>
@@ -33,11 +29,48 @@
 import { ref, onMounted, onUnmounted } from 'vue';
 
 const isScrolled = ref(false);
-const activeSection = ref(''); // Variable para almacenar la sección activa
+const activeSection = ref('about'); // Establecer la sección 'about' como la activa por defecto
 
 const handleScroll = () => {
-  // Verificar si el usuario ha hecho scroll más allá de cierta posición (por ejemplo, 50 píxeles desde la parte superior)
-  isScrolled.value = window.scrollY > 50;
+  // Verificar la posición del scroll en relación con cada sección de la página
+  const aboutSection = document.getElementById('about');
+  const resumeSection = document.getElementById('resume');
+  const portfolioSection = document.getElementById('portfolio');
+  const githubSection = document.getElementById('github');
+
+  const scrollY = window.scrollY;
+  const scrollTop = window.scrollY;
+  const headerHeight = 50; // Altura de la cabecera (ajusta según tu diseño)
+  
+  console.log('ScrollY',scrollY)
+
+  console.log('github',githubSection.offsetTop)
+
+  if (scrollTop < headerHeight) {
+    activeSection.value = 'about';
+  }
+
+  if (
+    scrollY >= aboutSection.offsetTop && 
+    scrollY < resumeSection.offsetTop
+  ) {
+    activeSection.value = 'about';
+  } else if (
+    scrollY >= resumeSection.offsetTop && 
+    scrollY < githubSection.offsetTop
+  ) {
+    activeSection.value = 'resume';
+  } else if (
+    scrollY >= githubSection.offsetTop && 
+    scrollY < portfolioSection.offsetTop
+  ) {
+    activeSection.value = 'github';
+  } else {
+    activeSection.value = 'portafolio';
+  }
+
+  // Actualizar el estado de la variable isScrolled
+  isScrolled.value = scrollY > 50;
 };
 
 onMounted(() => {
@@ -49,17 +82,14 @@ onUnmounted(() => {
   // Eliminar el evento de scroll cuando el componente se desmonta para evitar fugas de memoria
   window.removeEventListener('scroll', handleScroll);
 });
-
-// Función para establecer la sección activa cuando haces clic en un enlace del navbar
-const setActiveSection = (section) => {
-  activeSection.value = section;
-};
-
 </script>
 
 <style scoped>
-.navbar-link-active { /* Estilos para el enlace activo */
-  font-weight: bold;
+
+.navbar-link.navbar-link.active {
   color: var(--vegas-gold);
+  font-weight: bold;
 }
+
+/* Resto de tus estilos para el navbar aquí */
 </style>
