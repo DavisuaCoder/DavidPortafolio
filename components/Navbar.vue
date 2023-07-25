50393570eb102ad7c848ac3d299cc66a642d3bf6
David Alvarez <davisua.stuff@gmail.com>
2023-07-25 20:05:09 +0200

    Sticky navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar">
+  <nav class="navbar" :class="{ 'navbar-scroll': isScrolled }">
     <ul class="navbar-list">
       <li class="navbar-item">
         <a href="#about" class="navbar-link" :class="{ 'navbar-link-active': activeSection === 'about' }" @click="setActiveSection('about')">
@@ -30,9 +30,25 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 
+const isScrolled = ref(false);
 const activeSection = ref(''); // Variable para almacenar la sección activa
+
+const handleScroll = () => {
+  // Verificar si el usuario ha hecho scroll más allá de cierta posición (por ejemplo, 50 píxeles desde la parte superior)
+  isScrolled.value = window.scrollY > 50;
+};
+
+onMounted(() => {
+  // Escuchar el evento de scroll cuando el componente está montado
+  window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => {
+  // Eliminar el evento de scroll cuando el componente se desmonta para evitar fugas de memoria
+  window.removeEventListener('scroll', handleScroll);
+});
 
 // Función para establecer la sección activa cuando haces clic en un enlace del navbar
 const setActiveSection = (section) => {
